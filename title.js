ff46d59d8f3b42d63fbcbaf395a50c4aac79d39c
johnmorach <johnmorach.school@gmail.com>
2018-02-28 17:48:59 -0500

    Random ghost image

--- title.js
+++ title.js
@@ -1,8 +1,17 @@
 function NameGenerator() {
     if (Math.floor((Math.random() * 10) + 1) % 2 == 1) {
-    	document.getElementById("TitleText").innerHTML = "Ghost 👻 Registrar"
+    	document.getElementById("TitleText").innerHTML = document.getElementById("TitleText").innerHTML.replace("Registrar", "Register");
     }
-    else {
-    	document.getElementById("TitleText").innerHTML = "Ghost 👻 Register"
-    }
+}
+
+function ImageGenerator() {
+	var num = Math.floor(Math.random() * 7) + 1;
+	var img = document.getElementById("TitleImage");
+	
+	img.src = 'Ghosts/Ghost' + num + '.png';
+}
+
+function Generator() {
+	NameGenerator();
+	ImageGenerator();
 }
