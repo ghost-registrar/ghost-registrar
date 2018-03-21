6074725331b45482da0c86885fa4b936ba0d72d4
Samuel Breese <samuel@chame.co>
2018-03-21 18:32:34 -0400

    Finish preliminary webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+require('file-loader?name=[name].[ext]!../assets/index.html');
+
+import '../assets/stylesheets/index.css';
+import Ghost1 from '../assets/images/ghosts/Ghost1.png'
+import Ghost2 from '../assets/images/ghosts/Ghost2.png'
+import Ghost3 from '../assets/images/ghosts/Ghost3.png'
+import Ghost4 from '../assets/images/ghosts/Ghost4.png'
+import Ghost5 from '../assets/images/ghosts/Ghost5.png'
+import Ghost6 from '../assets/images/ghosts/Ghost6.png'
+import Ghost7 from '../assets/images/ghosts/Ghost7.png'
+
+import $ from 'jquery'
+
 function NameGenerator() {
 	var val = document.getElementById("TitleText").innerHTML;
 	if (Math.floor((Math.random() * 10) + 1) % 2 == 1) {
@@ -26,14 +39,16 @@ function CreditsGenerator() {
 }
 
 function ImageGenerator() {
-	var num = Math.floor(Math.random() * 7) + 1;
-	var img = document.getElementById("TitleImage");
-	
-	img.src = 'assets/images/ghosts/Ghost' + num + '.png';
-	img.height = 32;
-	img.width = 32;
-	
-	document.getElementById("PageIcon").href = 'assets/images/ghosts/Ghost' + num + '.png';
+    var num = Math.floor(Math.random() * 7);
+    var img = document.getElementById("TitleImage");
+
+    var ghosts = [Ghost1, Ghost2, Ghost3, Ghost4, Ghost5, Ghost6, Ghost7];
+
+    img.src = ghosts[num];
+    img.height = 32;
+    img.width = 32;
+
+    document.getElementById("PageIcon").href = 'assets/images/ghosts/Ghost' + num + '.png';
 }
 
 function Generator() {
@@ -41,3 +56,5 @@ function Generator() {
 	ImageGenerator();
 	CreditsGenerator();
 }
+
+$(Generator);
