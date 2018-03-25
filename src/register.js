00008cb0b8368ac5ccc2f240c7df53e71181888f
johnmorach <johnmorach.school@gmail.com>
2018-03-25 19:11:23 -0400

    Update js files to match standards

--- src/register.js
+++ src/register.js
@@ -1,38 +1,47 @@
 import $ from 'jquery';
 
+/** Sets up details for registration */
 function registerSubmit() {
-    document.getElementById("RegTime").innerHTML = document.getElementById("regTime").value;
-    document.getElementById("RegDate").innerHTML = document.getElementById("regDate").value;
-    
+    document.getElementById('RegTime').innerHTML =
+    document.getElementById('regTime').value;
+
+    document.getElementById('RegDate').innerHTML =
+    document.getElementById('regDate').value;
+
     // fill array with nonempty CRNs from input fields
-    var valid = [];
-    for (var i = 0; i < 6; i++) {
-        var idStr = "crn" + (i + 1);
+    let valid = [];
+    for (let i = 0; i < 6; i++) {
+        let idStr = 'crn' + (i + 1);
         if (document.getElementById(idStr).value.length != 0) {
             valid.push(document.getElementById(idStr).value);
         }
     }
-    
+
     if (valid.length > 0) {
-        var queryStr = "https://yacs.cs.rpi.edu/api/v5/sections.json?show_periods&crn=";
-        for (var i = 0; i < valid.length; i++) {
+        let queryStr = 'https://yacs.cs.rpi.edu/api/v5/sections.json?show_periods&crn=';
+        for (let i = 0; i < valid.length; i++) {
             queryStr += valid[i];
             if (i != valid.length - 1) {
-                queryStr += ",";
+                queryStr += ',';
             }
         }
-        
-        var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-        
+
+        let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+
         // use jQuery to get json from YACS api
         $.getJSON(queryStr, function(data) {
-            for (var i = 0; i < data.sections.length; i++) {
-                var idStr = "Course" + (i + 1);
-                var course = data.sections[i];
-                var courseText = "";
-                courseText += course.course_name + " (" + course.department_code + " " +  course.course_number + ")<br>";
-                for (var j = 0; j < course.periods.length; j++) {
-                    courseText += course.periods[j].type + ": " + days[course.periods[j].day - 1] + " " + course.periods[j].start + " - " + course.periods[j].end + "<br>";
+            for (let i = 0; i < data.sections.length; i++) {
+                let idStr = 'Course' + (i + 1);
+                let course = data.sections[i];
+                let courseText = '';
+                courseText += course.course_name + ' ('
+                + course.department_code + ' '
+                + course.course_number + ')<br>';
+                for (let j = 0; j < course.periods.length; j++) {
+                    courseText += course.periods[j].type + ': '
+                    + days[course.periods[j].day - 1] + ' '
+                    + course.periods[j].start + ' - '
+                    + course.periods[j].end + '<br>';
                 }
                 document.getElementById(idStr).innerHTML = courseText;
             }
@@ -40,4 +49,4 @@ function registerSubmit() {
     }
 }
 
-window.registerSubmit = registerSubmit
+window.registerSubmit = registerSubmit;
--- src/yacs.js
+++ src/yacs.js
@@ -1,43 +1,45 @@
 import $ from 'jquery';
 
+/** Imports CRNs from a YACS schedule link */
 function importFromYACS() {
     // should add paste from clipboard if possible
-    var url = window.prompt("Enter the schedule link (copied from YACS):", "");
-    
+    let url = window.prompt('Enter the schedule link (copied from YACS):', '');
+
     if (url == null) {
         return;
     }
-    
-    var regex = /https?:\/\/yacs\.cs\.rpi\.edu\/#\/schedules\?section_ids=/
-    var valid = regex.test(url);
-    
+
+    let regex = /https?:\/\/yacs\.cs\.rpi\.edu\/#\/schedules\?section_ids=/;
+    let valid = regex.test(url);
+
     if (!valid) {
-        window.alert("Invalid schedule link.")
+        window.alert('Invalid schedule link.');
     }
-    url = url.replace("#", "api/v5");
-    
+    url = url.replace('#', 'api/v5');
+
     $.getJSON(url, function(data) {
         if (data.schedules.length > 0) {
-            var index = 0;
-            
+            let index = 0;
+
             // match schedule_index to check if an index is specified
-            var scheduleIndex = url.match(/schedule_index=\d+/);
+            let scheduleIndex = url.match(/schedule_index=\d+/);
             if (scheduleIndex != null && scheduleIndex.length > 0) {
-                var str = scheduleIndex[0];
-                str = str.slice(str.indexOf("=") + 1);
+                let str = scheduleIndex[0];
+                str = str.slice(str.indexOf('=') + 1);
                 index = str;
             }
-            
-            var schedule = data.schedules[index];
-            for (var i = 0; i < schedule.sections.length && i < 6; i++) {
-                document.getElementById("crn" + (i + 1)).value = schedule.sections[i].crn;
+
+            let schedule = data.schedules[index];
+            for (let i = 0; i < schedule.sections.length && i < 6; i++) {
+                let crnField = document.getElementById('crn' + (i + 1));
+                crnField.value = schedule.sections[i].crn;
             }
             // might be able to reuse data from returned json
-            //RegisterSubmit();
+            // could register or at least show courses here
         } else {
-            window.alert("No schedules found.");
+            window.alert('No schedules found.');
         }
     });
 }
 
-window.importFromYACS = importFromYACS
+window.importFromYACS = importFromYACS;
