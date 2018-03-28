b8c2c0b60a5adef6ed999c1cf6acdc1fbc6c29e7
johnmorach <johnmorach.school@gmail.com>
2018-03-28 17:07:03 -0400

    Update JS to meet standards

--- src/sis.js
+++ src/sis.js
@@ -1,7 +1,8 @@
 import $ from 'jquery';
 
+/** Test POST request */
 function testPost() {
-    $.post("http://localhost:3000/sis/name", {rin: "rin_goes_here", pass: "password_goes_here"}, data => alert(data));
+    $.post('http://localhost:3000/sis/name', {rin: 'rin_goes_here', pass: 'password_goes_here'}, (data) => alert(data));
 }
 
 $(testPost);
