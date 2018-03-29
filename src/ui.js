0ccc4b2b03772b5b9cf7efc83c5e0e2d713619aa
Samuel Breese <samuel@chame.co>
2018-03-29 17:21:27 -0400

    small fixes

--- src/ui.js
+++ src/ui.js
@@ -1,20 +1,11 @@
 import $ from 'jquery';
 
-/** Injects a randomized title into the page. */
-function insertTitle() {
+$(window).on('load', () => {
     $('#register-registrar').html(
         Math.random() < 0.5 ? 'Register' : 'Registrar'
     );
-}
-
-/** Injects a randomized ghost icon into the page. */
-function insertIcon() {
     let colors = ['orange', 'blue', 'red', 'pink'];
     $('#ghost').css('color', colors[Math.floor(Math.random() * colors.length)]);
-}
-
-/** Injects the randomly-ordered project credits into the page. */
-function insertCredits() {
     let names = [
         ['John Morach', 'johnmorach'],
         ['Cameron Riley', 'camr315'],
@@ -26,11 +17,5 @@ function insertCredits() {
                        .sort((x, y) => Math.random() < 0.5 ? -1 : 1)
                        .map(([n, u]) => '<a href="https://github.com/' + u + '">' + n + '</a>')
                        .reduce((x, y) => x + ', ' + y));
-}
-
-$(window).on('load', () => {
-    insertTitle();
-    insertIcon();
-    insertCredits();
     $('#cover').fadeOut('slow');
 });
