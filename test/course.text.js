2828914d9b588dae70798c35d1554d268bd9aed3
RustyPotato <kylerfawcett@gmail.com>
2018-04-18 18:56:03 -0400

    Reread the docs, flipped the bools.

--- test/course.text.js
+++ test/course.text.js
@@ -31,7 +31,7 @@ describe('Course', function() {
     // These conflicts are largely based off the periods conflicting. So these tests are simple 
     // pass fail tests. 
     describe('conflicts', function() {
-        it('should return false if either course has conflicting periods within. ', function() {
+        it('should return true if either course has conflicting periods within. ', function() {
         
             let c1json = {"course_name": "Natural Sciences I",                "course_number": 1030, "department_code": "ERTH", "name": "01","periods": [{"type": "LEC","day": 1,"start": 1600,"end": 1750}, {"type": "LEC", "day": 4, "start": 1600, "end": 1750}]};
             let c2json = {"course_name": "Software Design and Documentation", "course_number": 4440, "department_code": "CSCI", "name": "01","periods": [{"type": "LEC","day": 1,"start": 1600,"end": 1750}, {"type": "LEC", "day": 4, "start": 1600, "end": 1750}]};
@@ -42,17 +42,17 @@ describe('Course', function() {
             let c3 = new Course(66666, c3json);
             
             // Makes sure it is reflexive
-            assert.ok(!c1.conflicts(c2), "Course conflict test 1 failed.");
-            assert.ok(!c2.conflicts(c1), "Course conflict test 2 failed.");
+            assert.ok( c1.conflicts(c2), "Course conflict test 1 failed.");
+            assert.ok( c2.conflicts(c1), "Course conflict test 2 failed.");
             
             // Makes sure there are no conflict paths.
-            assert.ok( c3.conflicts(c2), "Course conflict test 3 failed.");
-            assert.ok( c3.conflicts(c1), "Course conflict test 4 failed.");
+            assert.ok(!c3.conflicts(c2), "Course conflict test 3 failed.");
+            assert.ok(!c3.conflicts(c1), "Course conflict test 4 failed.");
             
             // A course must conflict with itself.
-            assert.ok(!c1.conflicts(c1), "Course conflict test 5 failed.");
-            assert.ok(!c2.conflicts(c2), "Course conflict test 6 failed.");
-            assert.ok(!c3.conflicts(c3), "Course conflict test 7 failed.");
+            assert.ok( c1.conflicts(c1), "Course conflict test 5 failed.");
+            assert.ok( c2.conflicts(c2), "Course conflict test 6 failed.");
+            assert.ok( c3.conflicts(c3), "Course conflict test 7 failed.");
             
         });
     });
