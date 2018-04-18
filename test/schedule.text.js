eee3ebb6d0ad0b2724d9ba5cefa021475143b5f4
RustyPotato <kylerfawcett@gmail.com>
2018-04-18 19:47:47 -0400

    Flipped boolean bug.

--- test/schedule.text.js
+++ test/schedule.text.js
@@ -33,10 +33,10 @@ describe('Schedule', function() {
             let s3 = new Schedule([]);
             let s4 = new Schedule([c1]);
             
-            assert.ok(s1.hasConflicts(), "Failed on hasConflicts() test 1.");
-            assert.ok(s2.hasConflicts(), "Failed on hasConflicts() test 2.");
-            assert.ok(s3.hasConflicts(), "Failed on hasConflicts() test 3.");
-            assert.ok(s4.hasConflicts(), "Failed on hasConflicts() test 4.");
+            assert.ok(!s1.hasConflicts(), "Failed on hasConflicts() test 1.");
+            assert.ok(!s2.hasConflicts(), "Failed on hasConflicts() test 2.");
+            assert.ok(!s3.hasConflicts(), "Failed on hasConflicts() test 3.");
+            assert.ok(!s4.hasConflicts(), "Failed on hasConflicts() test 4.");
             
         });
     });
